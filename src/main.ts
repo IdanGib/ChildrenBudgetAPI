8c0f5c96899c01fbd6195c60f34227c9208900e7
Idan <idangibly.dev@gmail.com>
2024-10-14 00:12:31 +0300

    test budgetchildren app update

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
-import { hello } from '@idangib/childrenbudget';
-hello();
+import { ChildrenBudget } from '@idangib/childrenbudget';
+import { DbClientsOptions } from '@idangib/childrenbudget/dist/interface/database.interface';
+const main = async () => {
+    const bc = await ChildrenBudget({ dbClientOption: DbClientsOptions.MY_SQL });
+    bc.budgets.create();
+}
+
+void main();
+
+
