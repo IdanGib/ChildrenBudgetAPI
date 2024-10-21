d00aeb704ec3ca2e9939ee6e8d84c15963cbf597
Idan <idangibly.dev@gmail.com>
2024-10-21 21:03:18 +0300

    test middlewares

--- src/api/api.middleware.ts
+++ src/api/api.middleware.ts
@@ -1,6 +1,14 @@
+import { json } from 'body-parser';
+import compression from 'compression';
 import { Express } from 'express';
+import cookie from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
 export const middleware = async (app: Express) => {
-   app.use((req, res, next) => {
-      next();
-   });
+   app.use(json());
+   app.use(cookie());
+   app.use(cors());
+   app.use(morgan('tiny'));
+   app.use(compression());
 }
