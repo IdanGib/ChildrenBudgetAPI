1a84ce670740f1f5d9d0ff24ead8495217ce2e33
Idan <idangibly.dev@gmail.com>
2024-10-31 10:38:20 +0200

    setup docker

--- src/lib/env.config.ts
+++ src/lib/env.config.ts
@@ -1,5 +1,15 @@
+import { readFileSync } from 'fs';
 import { IEnvConfig } from "@/interface/env.interface";
 const env = process.env;
+
+
+const loadFileContent = (path?: string): string => {
+    if (path) {
+        return readFileSync(path, 'utf-8').toString();
+    }
+    return '';
+}
+
 class EnvConfig implements IEnvConfig {
     public readonly NPMRC_GITHUB_PACKAGES_AUTH_LINE: string = env.NPMRC_GITHUB_PACKAGES_AUTH_LINE ?? '';
     public readonly DB_POSTGRESQL_HOST: string = env.DB_POSTGRESQL_HOST ?? '';
@@ -8,7 +18,7 @@ class EnvConfig implements IEnvConfig {
     public readonly DB_POSTGRESQL_USERNAME: string = env.DB_POSTGRESQL_USERNAME ?? '';
     public readonly DB_POSTGRESQL_DATABASE: string = env.DB_POSTGRESQL_DATABASE ?? '';
     public readonly API_PORT: number = Number(env.API_PORT)
-    public readonly AUTH_SECRET: string = env.AUTH_SECRET ?? '';
+    public readonly AUTH_SECRET: string = env.AUTH_SECRET ?? loadFileContent(env.AUTH_SECRET_FILE);
     public readonly AUTH_GOOGLE_REDIRECT: string = env.AUTH_GOOGLE_REDIRECT ?? '';
     public readonly AUTH_GOOGLE_ID: string = env.AUTH_GOOGLE_ID ?? '';
     public readonly AUTH_GOOGLE_SECRET: string = env.AUTH_GOOGLE_SECRET ?? '';
--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express';
+import { envConfig } from './lib/env.config';
+import { EnvFields } from './interface/env.interface';
 const test = async () => {
     const app = express();
     app.get('/:key', (req: Request, res: Response) => {
         const { key } = req.params;
-        console.log(req.params);
-        res.json({ [key]: process.env[key] ?? '' });
+        res.json({ [key]: envConfig[key as EnvFields] ?? '' });
     });
     app.get('/',  (req: Request, res: Response) => {
         res.json({ message: 'hello' });
